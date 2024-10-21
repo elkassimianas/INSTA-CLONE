6759740fe26aceae6073f288f8fea34fc9ac55e6
ael-kass <aelkassimi321@gmail.com>
2024-10-21 10:28:13 +0100

    finish the HomePage & create a router for the ProfilePage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/auth' element={<AuthPage />} />
+        <Route path='/:username' element={<ProfilePage />} />
       </Routes>
     </PageLayout>
   )
