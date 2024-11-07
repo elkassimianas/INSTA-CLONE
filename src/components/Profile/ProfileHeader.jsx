b53912979fda57099867211093d4bfe5ae018cb4
ael-kass <aelkassimi321@gmail.com>
2024-11-07 11:23:06 +0100

    create a Modal to Edit User Profile && handle the Edit User Profile feature

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,6 +1,7 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import { Avatar, AvatarGroup, Button, Flex, Text, useDisclosure, VStack } from "@chakra-ui/react"
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore"
+import EditProfile from "./EditProfile";
 
 const ProfileHeader = () => {
 
@@ -8,6 +9,7 @@ const ProfileHeader = () => {
     const authUser = useAuthStore(state => state.user);
     const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
     const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
   return <Flex gap={{base:4, sm:10}} py={10} direction={{base:"column", sm:"row"}}>
     <AvatarGroup size={{base:"xl", md:"2xl"}} justifySelf={"center"} alignSelf={"self-start"}
@@ -26,7 +28,7 @@ const ProfileHeader = () => {
             {visitingOwnProfileAndAuth && (
                 <Flex gap={4} alignItems={"center"} justifyContent={"center"} >
                     <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}}
-                        size={{base:"xs", md:"sm"}}
+                        size={{base:"xs", md:"sm"}} onClick={onOpen}
                     >
                         Edit Profile
                     </Button>
@@ -70,6 +72,7 @@ const ProfileHeader = () => {
         </Flex>
         <Text fontSize={"sm"} >{userProfile.bio}</Text>
     </VStack>
+    {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
   </Flex>
 }
 
